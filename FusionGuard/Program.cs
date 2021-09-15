26d21a854fbcee3350f6479106754dd97b51dfd9
Michele Beckmann <m.beckmann@live.de>
2021-09-15 23:47:59 +0200

    add CommandHandler

--- FusionGuard/Program.cs
+++ FusionGuard/Program.cs
@@ -3,7 +3,7 @@ using FusionGuard.Twitch;
 
 var config = ConfigReader.Read();
 
-using var bot = new TwitchBot(config.TwitchUsername, config.OAuthKey, config.TwitchUsername))
+using var bot = new TwitchBot(config.TwitchUsername, config.OAuthKey, config.TwitchUsername);
 bot.Start();
 while(bot.IsRunning)
 {
--- FusionGuard/Twitch/TwitchBot.cs
+++ FusionGuard/Twitch/TwitchBot.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FusionGuard.Twitch.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 using TwitchLib.Client;
 using TwitchLib.Client.Events;
 using TwitchLib.Client.Models;
+using TwitchLib.Client.Extensions;
 using TwitchLib.Communication.Clients;
 using TwitchLib.Communication.Models;
 
@@ -14,11 +16,15 @@ namespace FusionGuard.Twitch
     public class TwitchBot : IDisposable
     {
         TwitchClient _client;
+        string _defaultChannelName;
+        DefaultChannelCommandHandler _defaultChannelCommandHandler;
 
         public bool IsRunning { get; private set; }
 
         public TwitchBot(string twitchUsername, string oAuthKey, string defaultChannelName)
         {
+            _defaultChannelCommandHandler = new DefaultChannelCommandHandler();
+            _defaultChannelName = defaultChannelName;
             _client = new TwitchClient(new WebSocketClient(new ClientOptions { MessagesAllowedInPeriod = 750, ThrottlingPeriod = TimeSpan.FromSeconds(30) }));
             _client.Initialize(new ConnectionCredentials(twitchUsername, oAuthKey), defaultChannelName);
             RegisterEvents();
@@ -38,17 +44,25 @@ namespace FusionGuard.Twitch
 
         private void RegisterEvents()
         {
-            _client.OnLog += Client_OnLog;
+            _client.OnLog += Log;
+            _client.OnMessageReceived += HandleMessageReceived;
         }
 
         public void Dispose()
         {
-            _client.OnLog -= Client_OnLog;
+            _client.OnLog -= Log;
+            _client.OnMessageReceived -= HandleMessageReceived;
         }
 
-        private void Client_OnLog(object? sender, OnLogArgs e)
+        private void Log(object? sender, OnLogArgs e)
         {
-            Console.WriteLine($"{e.DateTime.ToString()}: {e.BotUsername} - {e.Data}");
+            Console.WriteLine($"{e.DateTime}: {e.BotUsername} - {e.Data}");
+        }
+
+        private void HandleMessageReceived(object? sender, OnMessageReceivedArgs e)
+        {         
+            if(e.ChatMessage.Channel == _defaultChannelName)
+                _defaultChannelCommandHandler.Handle(e.ChatMessage.Message);
         }
     }
 }

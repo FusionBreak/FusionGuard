c71d7ce89cfc6a2854e7393f23b80abbf7f56706
Michele Beckmann <m.beckmann@live.de>
2021-11-06 16:09:31 +0100

    update database context

---
+++ FusionGuard/Migrations/20211106145757_panicfollows.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace FusionGuard.Migrations
+{
+    public partial class panicfollows : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PanicFollow",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PanicId = table.Column<int>(type: "integer", nullable: true),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    TwitchUserId = table.Column<string>(type: "text", nullable: false),
+                    FollowedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PanicFollow", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PanicFollow_Panic_PanicId",
+                        column: x => x.PanicId,
+                        principalTable: "Panic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PanicFollow_PanicId",
+                table: "PanicFollow",
+                column: "PanicId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PanicFollow");
+        }
+    }
+}
